3a14b5ee464eb7a0a30632a0c782169885ef1736
Taylor Smith <tsmith1024@gmail.com>
2021-11-05 10:55:47 -0500

    Initial Commit

---
+++ AppStorageCounter/AppStorageCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStorageCounterApp.swift
+//  AppStorageCounter
+//
+//  Created by Taylor Smith on 11/5/21.
+//
+
+import SwiftUI
+
+@main
+struct AppStorageCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
